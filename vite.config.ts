290c8c053915ec842822c47b9bef76418c3d4c83
HjET <11130582+HjETWD@users.noreply.github.com>
2025-02-10 00:02:57 +0300

    add alias to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pet.react.todolist/",
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+	base: "/pet.react.todolist/",
+	plugins: [
+		react(),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+			'@components': resolve(__dirname, './src/components'),
+		}
+	},
 })
