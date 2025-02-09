9165d8b5c1d28efa4ce85e741843c3c6892a5b4e
HjET <11130582+HjETWD@users.noreply.github.com>
2025-02-10 02:32:26 +0300

    set beautiful some styles + noscript

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import './index.css'
 
 function App() {
 	return (
-		<div className='flex min-h-screen font-serif transition-colors dark:bg-gray-900 dark:text-white'>
-			<div className="m-auto">
-				<h1>TODO List</h1>
-			</div>
+		<div className='grid place-items-center min-h-screen font-noto transition-colors dark:bg-gray-900 dark:text-white'>
+			<h1>TODO List</h1>
 		</div>
 	)
 }
