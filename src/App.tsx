1abf093fdc22406ce853d66b7d181ef525329839
HjET <11130582+HjETWD@users.noreply.github.com>
2025-02-10 02:19:37 +0300

    add base dark themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './index.css'
 
 function App() {
 	return (
-		<div className='flex h-screen'>
+		<div className='flex min-h-screen font-serif transition-colors dark:bg-gray-900 dark:text-white'>
 			<div className="m-auto">
 				<h1>TODO List</h1>
 			</div>
